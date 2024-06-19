7107abd3788b8e40f5c2e033a935a077c1be2e28
Steve Foley <steve.foley@intercom.io>
2024-06-19 10:51:25 +0100

    Click counter

--- app/components/root.js
+++ app/components/root.js
@@ -1,5 +1,19 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 
 export default class RootComponent extends Component {
+
+  @tracked state = [{count: 0}];
+
+  @action
+  handleClick () {
+    this.state[0].count += 1;
+    this.state = this.state;
+  }
+
+  get count () {
+    return this.state[0].count;
+  }
 }
