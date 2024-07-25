99e626d8aac2efa796ffceb6c66e5c4e032934c5
Steve Foley <steve.foley@intercom.io>
2024-07-25 14:26:25 +0100

    something loading

--- app/routes/something.js
+++ app/routes/something.js
@@ -1,3 +1,25 @@
 import Route from '@ember/routing/route';
 
-export default class SomethingRoute extends Route {}
+export default class SomethingRoute extends Route {
+  async beforeModel() {
+    console.log('beforeModel.before');
+    await new Promise(r => setTimeout(r, 500));
+    console.log('beforeModel.after');
+  }
+
+  async model() {
+    const n = 500;
+    console.log('model.before');
+    await new Promise(r => setTimeout(r, n));
+    console.log('model.after');
+    return {
+      n,
+    };
+  }
+
+  async afterModel() {
+    console.log('afterModel.before');
+    await new Promise(r => setTimeout(r, 500));
+    console.log('afterModel.after');
+  }
+}
